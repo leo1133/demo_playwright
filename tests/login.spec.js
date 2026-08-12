34fd555cc522451b9f6895988f04b426aeae6439
leo.1133 <ngaphuongtb123@gmail.com>
2026-08-12 16:58:36 +0700

    update login testcase

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -65,4 +65,28 @@ test.describe("Login", () => {
 
     await expect(page.getByText("Invalid email or password")).toBeVisible();
   });
+
+  test("Check login with checkbox Remember me = checked", async ({ page }) => {
+    await page.goto("https://crm.anhtester.com/admin/authentication");
+    await page.getByRole("textbox", { name: "Email Address" }).fill("admin@example.com");
+    await page.getByRole("textbox", { name: "Password" }).fill("123456");
+    await page.getByRole("checkbox", { name: "Remember me" }).check();
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page).toHaveURL(/admin/);
+  });
+
+  test("Check login with checkbox Remember me = unchecked", async ({ page }) => {
+    await page.goto("https://crm.anhtester.com/admin/authentication");
+    await page.getByRole("textbox", { name: "Email Address" }).fill("admin@example.com");
+    await page.getByRole("textbox", { name: "Password" }).fill("123456");
+    await page.getByRole("checkbox", { name: "Remember me" }).uncheck();
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page).toHaveURL(/admin/);
+  });
+
+  test("Check when click button Forgot password", async ({ page }) => {
+    await page.goto("https://crm.anhtester.com/admin/authentication");
+    await page.getByRole("link", { name: "Forgot Password?" }).click();
+    await expect(page.getByRole("heading", { name: "Forgot Password" })).toBeVisible();
+  });
 });
